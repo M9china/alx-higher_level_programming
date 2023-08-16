b9fd4c8b8ac67813197b3b3155a60cf1dbef0649
M9china <nqobilencaza2@gmail.com>
2023-08-16 14:24:01 -0700

    adds all unique intergers in array

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    unique_sum = 0
+    seen = set()
+
+    for num in my_list:
+        if num not in seen:
+            unique_sum += num
+            seen.add(num)
+
+    return unique_sum
