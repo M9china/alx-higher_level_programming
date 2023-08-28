f189aee8e2d4aa8aef0d5d3d8a2979934b05e308
M9china <nqobilencaza2@gmail.com>
2023-08-28 15:51:35 -0700

    safe print file

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0;
+        for item in my_list:
+            if count < x:
+                print(item, end='')
+                count += 1
+        print()
+        return count
+    except:
+        print()
+        return count
+
