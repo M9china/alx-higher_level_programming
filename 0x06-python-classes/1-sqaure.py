5900f81239d9412287f043a5a81029b071e964c6
M9china <nqobilencaza2@gmail.com>
2023-08-31 16:01:09 +0000

    defines a sqaure

---
+++ 0x06-python-classes/1-sqaure.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Write a class Square that defines a square"""
+
+
+class Square:
+    """defines new square"""
+
+    def __init__(val, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        val.__size = size
