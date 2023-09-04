efbf6a6240961feb0a9652a12e19c95fe5f7206a
M9china <nqobilencaza2@gmail.com>
2023-09-04 19:16:38 +0000

    removed white spce

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-""" defines a class sqaure """
+"""empty class Square that defines a square"""
 
 
 class Square:
-	""" empty square """
-	pass
+    """Represents a square"""
+    pass
