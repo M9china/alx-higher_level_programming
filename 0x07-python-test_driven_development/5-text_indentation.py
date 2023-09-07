39a3b0d1172e8c0f48d4e1900d2a1e0b5c1d8fdb
M9china <nqobilencaza2@gmail.com>
2023-09-07 16:46:20 +0000

    prints 2 lines after each character

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,35 @@
 #!/usr/bin/python3
-"""
-This is the "4-print_square" module.
-The 4-print_square  module supplies one function, print_square(size).
-"""
 
+def text_indentation(text):
+    """
+    Print the input text with two new lines after each '.', '?', and ':' characters.
+    
+    :param text: A string to be processed.
+    :type text: str
+    :raises TypeError: If the input is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
 
-def print_square(size):
-    """prints a square with "#"'s that has a length of size """
-    if type(size) is not int:
-        raise TypeError("size must be an integer")
-    if size < 0:
-        raise ValueError("size must be >= 0")
-    if size > 0:
-        print(("#" * size + "\n") * size, end="")
+    lines = text.split("\n")
+
+    for line in lines:
+        line = line.strip() 
+        if line:  
+            result = ""
+            newline = True 
+
+            for char in line:
+                if char in ".?:":
+                    result += char + "\n\n" 
+                    newline = True
+                elif char != ' ' and newline: 
+                    result += char
+                    newline = False
+                elif char != ' ':
+                    result += char
+
+            print(result)
+
+text = "This is a test. Another test? Yes: it works."
+text_indentation(text)
