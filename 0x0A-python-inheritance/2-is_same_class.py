5b901109ab82b04037238529c9c730280e26866d
M9china <95371889+M9china@users.noreply.github.com>
2023-09-13 18:20:59 +0200

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/2-is_same_class.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""holds the function is_same_class"""
-
-
-def is_same_class(obj, a_class):
-    """return true if obj is the exact class a_class, else false"""
-    return (type(obj) == a_class)
--- 0x0A-python-inheritance/5-base_geometry.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""holds the class BaseGeometry"""
-
-
-class BaseGeometry:
-    """An empty class"""
-    pass
