c429134ff785e1517908bf7db1e191a6d3e0640b
M9china <nqobilencaza2@gmail.com>
2023-10-11 13:13:39 -0700

    documented module

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,6 @@
 
 
 def write_file(filename="", text=""):
-    """returns the number of chars written to "filename" from "text" """
+    """"returns the number of chars written to "filename" from "text" """
     with open(filename, 'w', encoding='utf=8') as f:
         return f.write(text)
