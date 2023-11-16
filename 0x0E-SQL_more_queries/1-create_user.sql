3af1d48c45fa834cf94c4de9148e00c5caf774b5
M9china <nqobilencaza2@gmail.com>
2023-11-16 12:18:05 -0800

    fix:indentation

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- creates the user with all privileges
 CREATE USER
-IF NOT EXIXTS 'user_0d-1'@'localhost'
+	IF NOT EXIXTS 'user_0d-1'@'localhost'
 	IDENTIFIED BY 'user_0d_1_pwd';
-	GRANT ALL PRIVILEGES
+GRANT ALL PRIVILEGES
 	ON *.*
 	TO 'user_0d_1'@'localhoest';
