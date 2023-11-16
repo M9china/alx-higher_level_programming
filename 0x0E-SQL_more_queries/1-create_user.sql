603bf0b8d897c1c0e034604c4a0d6ad99531eb4f
M9china <nqobilencaza2@gmail.com>
2023-11-16 14:37:21 -0800

    fix: syntax

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- creates the user with all privileges
 CREATE USER
-	IF NOT EXIXTS 'user_0d-1'@'localhost'
-	IDENTIFIED BY 'user_0d_1_pwd';
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES
-	ON *.*
-	TO 'user_0d_1'@'localhoest';
+   ON *.*
+   TO 'user_0d_1'@'localhost';
