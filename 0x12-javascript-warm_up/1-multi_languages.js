74e5fddc8938fec22833cd79d6c27044816fb959
M9china <nqobilencaza2@gmail.com>
2024-01-08 17:32:23 +0200

    fix:semistandard validation

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -2,6 +2,6 @@
 
 const list = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
-for (let i = 0; i < 3; i++){
-    console.log(list[i]);
+for (let i = 0; i < 3; i++) {
+  console.log(list[i]);
 }
