4c4cc973cd27c7f1773ba108679477e480cf3396
M9china <nqobilencaza2@gmail.com>
2024-01-21 16:41:50 +0200

    fix: semistandard validation

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 
-
 class Rectangle {
-    constructor (w, h) {
-        this.width = w;
-        this.hight = h;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.hight = h;
+  }
 }
 
-const myRectangle = new Rectangle (5, 2);
+const myRectangle = new Rectangle(5, 2);
+
+console.log(`width is: ${myRectangle.width}`);
