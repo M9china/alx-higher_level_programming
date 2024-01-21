8fcdb2d68b28564a98f89f1bfde6b77d2784b75c
M9china <nqobilencaza2@gmail.com>
2024-01-21 18:18:12 +0200

    added white space

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,7 @@
 class Rectangle {
   constructor (w, h) {
     if (!Number.isInteger(w) || !Number.isInteger(h) || w <= 0 || h <= 0) {
-      process.stdout.write('Rectangle');
+      process.stdout.write('Rectangle ');
       return {};
     } else {
       this.width = w;
