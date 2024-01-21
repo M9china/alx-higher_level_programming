b51108b666ad6cd972cf7233faae52eb796107de
M9china <nqobilencaza2@gmail.com>
2024-01-21 16:50:48 +0200

    cleaned dead code

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -7,10 +7,5 @@ class Rectangle {
   }
 }
 
-const myRectangle = new Rectangle(3, 3);
-
-console.log(`width is: ${myRectangle.width}`);
-console.log(`hight is: ${myRectangle.height}`);
-
 module.exports = Rectangle;
 
