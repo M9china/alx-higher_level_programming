cbd4e49cb7073d84bd7a75efa0726d7b32020e94
M9china <nqobilencaza2@gmail.com>
2024-01-21 21:39:43 +0200

    hot fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const myModule = require('myModule');
+const fs = require('fs');
 
 const firstArg = myModule.readFileSync(process.argv[2]).toString();
 const secondArg = myModule.readFileSync(process.argv[3]).toString();
-myModule.writeFileSync(process.argv[4], firstArg + secondArg);
+fs.writeFileSync(process.argv[4], firstArg + secondArg);
