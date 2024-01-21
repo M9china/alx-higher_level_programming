144450f9b1c34f593675d66d18e860ed2b8eeb93
M9china <nqobilencaza2@gmail.com>
2024-01-21 18:15:55 +0200

    fix: semistandard validation

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (!Number.isInteger(w) || !Number.isInteger(h) || w <= 0 || h <= 0) {
-      process.stdout.write("Rectangle ");
+      process.stdout.write('Rectangle');
       return {};
     } else {
       this.width = w;
