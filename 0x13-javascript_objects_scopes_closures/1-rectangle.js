9434d5b699d31e36b4b27fa339b861b38f11d817
M9china <nqobilencaza2@gmail.com>
2024-01-21 16:44:43 +0200

    fix:typo

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -3,10 +3,12 @@
 class Rectangle {
   constructor (w, h) {
     this.width = w;
-    this.hight = h;
+    this.height = h;
   }
 }
 
 const myRectangle = new Rectangle(3, 3);
 
 console.log(`width is: ${myRectangle.width}`);
+console.log(`hight is: ${myRectangle.height}`);
+
