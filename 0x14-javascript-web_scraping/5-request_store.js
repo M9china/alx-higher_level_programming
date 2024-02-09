7ea02700adb83d5e5bd28851593fe66f47d1b095
M9china <nqobilencaza2@gmail.com>
2024-02-09 03:22:16 +0200

    hot fix

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,5 +1,12 @@
 #!/usr/bin/node
 
-const myModule = require('myModule');
 const request = require('request');
-request(process.argv[2]).pipe(myModule.createWriteStream(process.argv[3]));
+const fs = require('fs');
+
+request(process.argv[2], function (_err, _res, body) {
+  fs.writeFile(process.argv[3], body, 'utf8', function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+});
